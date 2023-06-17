62710f6f95e88ac8f471e7bae0b5e00306fef7b4
joao carlos <joaocarlosnnx@gmail.com>
2023-06-17 13:55:32 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    if (this.length === 0) {
+      this.items.push(item);
+    } else {
+      let index = 0;
+      while (index < this.length && this.items[index] <= item) {
+        index += 1;
+      }
+      this.items.splice(index, 0, item);
+    }
+    this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
 
-  min() {}
+    if (pos > this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
 
-  sum() {}
+  }
 
-  avg() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.length - 1];
+
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
+
+  sum() {
+    const suma = this.items.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
+
+    return suma;
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    const promedio = this.sum() / this.items.length;
+    return promedio;
+  }
 }
 
 module.exports = SortedList;
